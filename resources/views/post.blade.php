f16663a97adec7308086cf3f372b803d76412a70
Léo Reynal <leornl@mbp-de-leo.home>
2022-07-12 15:33:36 +0200

    Episode 15 complete

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
 
-<body>
+<x-layout>
     <article>
 
         <h1>{{ $post->title }}</h1>
@@ -19,6 +10,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+{{-- @extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
+@section('content')
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
+            <div> {{ $post->exerpt }} </div>
+        </article>
+    @endforeach
+@endsection
+ --}}
 
-<body>
+
+
+<x-layout>
 
     @foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'foobar' : '' }}">
-        <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
-        <div> {{ $post->exerpt }} </div>
-    </article>
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
+            <div> {{ $post->exerpt }} </div>
+        </article>
     @endforeach
 
-</body>
-
-</html>
+</x-layout>
