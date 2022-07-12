0a4cb14d7068818cf5ec61ae860e3002b9b0cf61
Léo Reynal <leornl@mbp-de-leo.home>
2022-07-12 17:11:43 +0200

    Episode 20 complete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,69 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use \Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-
-    public $title;
-
-    public $slug;
-
-    public $exerpt;
-
-    public $date;
-
-    public $body;
-
-    public function __construct($title, $exerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->exerpt = $exerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(function ($file) {
-                    return YamlFrontMatter::parseFile($file);
-                })
-                ->map(function ($document) {
-                    return new Post(
-                        $document->title,
-                        $document->exerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
-
-
-    public static function find($slug)
-    {
-
-        return static::all()->firstWhere('slug', $slug);
-
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if(! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    use HasFactory;
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -16,7 +16,7 @@
 
     @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
+            <h1><a href="/posts/{{ $post->id }}"> {{ $post->title }}</a></h1>
             <div> {{ $post->exerpt }} </div>
         </article>
     @endforeach
