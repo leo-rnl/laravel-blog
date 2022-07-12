767c5b9376ec1b7665d47a4b30c653bfb8f84a54
Léo Reynal <leornl@mbp-de-leo.home>
2022-07-12 15:16:02 +0200

    Episode 14 complete

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,15 @@
     <link rel="stylesheet" href="/app.css">
     <title>My Blog</title>
 </head>
+
 <body>
 
-    <?php foreach ($posts as $post) : ?>
-    <article>
-       <h1><a href="/posts/<?= $post->slug ?>"><?= $post->title ?></a></h1>
-       <div><?= $post->exerpt ?></div>
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+        <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
+        <div> {{ $post->exerpt }} </div>
     </article>
-    <?php endforeach ?>
+    @endforeach
 
 </body>
 
