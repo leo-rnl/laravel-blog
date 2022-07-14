2b161e022f46f3de4c59705928aa01a82fb18f37
Léo Reynal <leornl@mbp-de-leo.home>
2022-07-14 19:10:33 +0200

    Episode 33 complete

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,20 @@
-{{-- @extends('layout')
-
-@section('content')
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a></h1>
-            <div> {{ $post->exerpt }} </div>
-        </article>
-    @endforeach
-@endsection --}}
-
-
-
 <x-layout>
+    @include('_posts-header')
 
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>{{ $post->exerpt }}</div>
-        </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p>No posts yet. Please check later.</p>
+        @endif
+
+
+        {{-- <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+        </div> --}}
+    </main>
 
 </x-layout>
