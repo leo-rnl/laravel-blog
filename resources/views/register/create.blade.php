e8d7669baa6bbb0090222d67148cbccdaa0190cd
leornl <leoreynal.pro@gmail.com>
2022-07-16 12:00:12 +0200

    Episode 47 complete

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -7,7 +7,7 @@
             <form class="mt-10" method="POST" action="/register">
                 @csrf
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"  for="name">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
                         Name
                     </label>
                     <input
@@ -15,11 +15,15 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"  for="username">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
                         Username
                     </label>
                     <input
@@ -27,11 +31,15 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"  for="email">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
                         Email
                     </label>
                     <input
@@ -39,11 +47,15 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"  for="password">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
                         Password
                     </label>
                     <input
@@ -53,14 +65,28 @@
                         id="password"
                         required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
+                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
                     >
                         Submit
                     </button>
                 </div>
+
+                @if($errors->any())
+
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{$error}}</li>
+                        @endforeach
+                    </ul>
+
+                @endif
+
             </form>
         </main>
     </section>
