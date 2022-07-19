ec187b5a64353aa76083c19f370b1e2c2ed407d9
leornl <leoreynal.pro@gmail.com>
2022-07-19 14:47:17 +0200

    Episode 52 complete

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,23 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+
+    <div class="flex-shrink-0">
+        <img class="rounded-xl" src="https://i.pravatar.cc/60" alt="avatar">
+    </div>
+
+    <div class="">
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 month ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, atque aut blanditiis
+            commodi consectetur corporis dicta, dolor earum eum id laborum maxime nesciunt
+            perspiciatis praesentium quaerat quibusdam repudiandae similique temporibus!
+        </p>
+    </div>
+
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,7 +16,7 @@
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
                             <h5 class="font-bold">
-                                <a href="?author={{ $post->author->username  }}" >{{$post->author->name}}</a>
+                                <a href="?author={{ $post->author->username  }}">{{$post->author->name}}</a>
                             </h5>
                         </div>
                     </div>
@@ -52,6 +52,14 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment></x-post-comment>
+                    <x-post-comment></x-post-comment>
+                    <x-post-comment></x-post-comment>
+                    <x-post-comment></x-post-comment>
+                </section>
+
             </article>
         </main>
     </section>
