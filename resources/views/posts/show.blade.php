b658d8649b20618bdab993faf65901bc9b435c2e
leornl <leoreynal.pro@gmail.com>
2022-07-23 11:43:13 +0200

    Episode 56 complete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -54,35 +54,47 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-pannel>
+                    @auth
 
-                        <form method="POST" action="#">
-                            @csrf
+                        <x-pannel>
 
-                            <header class="flex">
-                                <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ auth()->id() }}"
-                                     alt="avatar">
+                            <form method="POST" action="/posts/{{ $post->slug  }}/comments">
+                                @csrf
 
-                                <h2>
-                                    Want to participate ?
-                                </h2>
+                                <header class="flex space-x-4 items-center">
+                                    <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ auth()->id() }}"
+                                         alt="avatar">
 
-                            </header>
-                            <div class="mt-6">
-                                <textarea name="body" id="body" class="w-full text-sm focus:outline-none focus:ring"
-                                          cols="30" rows="6"
-                                          placeholder="Quick, thing of something to say!"></textarea>
-                            </div>
+                                    <h2>
+                                        Want to participate ?
+                                    </h2>
 
-                            <div class="flex justify-end mt-10 border-t border-gray-200 pt-6 ">
-                                <button
-                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 hover:bg-blue-600 rounded-2xl"
-                                    type="submit">Post
-                                </button>
-                            </div>
+                                </header>
+                                <div class="mt-6">
+                                <textarea
+                                    name="body"
+                                    id="body"
+                                    class="w-full text-sm focus:outline-none focus:ring"
+                                    cols="30" rows="6"
+                                    placeholder="Quick, thing of something to say!"></textarea>
+                                </div>
 
-                        </form>
-                    </x-pannel>
+                                <div class="flex justify-end mt-10 border-t border-gray-200 pt-6 ">
+                                    <button
+                                        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 hover:bg-blue-600 rounded-2xl"
+                                        type="submit">Post
+                                    </button>
+                                </div>
+
+                            </form>
+                        </x-pannel>
+                    @else
+
+                        <p class="font-semibold">
+                            <a href="/login" class="hover:underline">Log in</a> or <a href="/register" class="hover:underline">register</a> to leave a comment!
+                        </p>
+
+                    @endauth
 
                     @foreach($post->comments as $comment)
                         <x-post-comment :comment="$comment"></x-post-comment>
